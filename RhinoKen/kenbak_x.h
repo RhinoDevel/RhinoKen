67c6b6bf902584ffcace44568a45caa82329985c
Marc <RhinoDevel@users.noreply.github.com>
2025-05-10 21:14:21 +0200

    Fix comment.

--- RhinoKen/kenbak_x.h
+++ RhinoKen/kenbak_x.h
@@ -11,7 +11,7 @@ enum kenbak_x
     kenbak_x_none = 0, // None of the signals/states below was detected last.
     kenbak_x_1 = 1, // If signal DA was last. <=> Display address mode.
     kenbak_x_2 = 2, // If signal DD was last. <=> Display memory mode.
-    kenbak_x_3 = 3, // If state is SA was last. <=> "Run mode" (see page 06!).
+    kenbak_x_3 = 3, // If state is SA. <=> "Run mode" (see page 06!).
     kenbak_x_4 = 4 // If signals BU or CL were last. <=> Input mode.
 };
 
