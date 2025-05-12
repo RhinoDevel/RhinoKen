e391e6ac996aa8b611ec590f6ebc1663e41aba69
Marc <RhinoDevel@users.noreply.github.com>
2025-05-12 20:28:12 +0200

    kenbak_state: Formatting.

--- RhinoKen/kenbak_state.c
+++ RhinoKen/kenbak_state.c
@@ -10,132 +10,39 @@ char const * kenbak_state_get_str(enum kenbak_state const state)
 {
 	switch(state)
 	{
-		case kenbak_state_power_off:
-		{
-			return "power off";
-		}
-		case kenbak_state_unknown:
-		{
-			return "unknown";
-		}
+		case kenbak_state_power_off:	{ return "po"; }
+		case kenbak_state_unknown:		{ return "un"; }
 
-		case kenbak_state_qb:
-		{
-			return "QB";
-		}
-		case kenbak_state_qc:
-		{
-			return "QC";
-		}
-		case kenbak_state_qd:
-		{
-			return "QD";
-		}
-		case kenbak_state_qe:
-		{
-			return "QE";
-		}
-		case kenbak_state_qf:
-		{
-			return "QF";
-		}
+		case kenbak_state_qb:			{ return "QB"; }
+		case kenbak_state_qc:			{ return "QC"; }
+		case kenbak_state_qd:			{ return "QD"; }
+		case kenbak_state_qe:			{ return "QE"; }
+		case kenbak_state_qf:			{ return "QF"; }
 
-		case kenbak_state_sa:
-		{
-			return "SA";
-		}
-		case kenbak_state_sb:
-		{
-			return "SB";
-		}
-		case kenbak_state_sc:
-		{
-			return "SC";
-		}
-		case kenbak_state_sd:
-		{
-			return "SD";
-		}
-		case kenbak_state_se:
-		{
-			return "SE";
-		}
-		case kenbak_state_sf:
-		{
-			return "SF";
-		}
-		case kenbak_state_sg:
-		{
-			return "SG";
-		}
-		case kenbak_state_sh:
-		{
-			return "SH";
-		}
-		case kenbak_state_sj:
-		{
-			return "SJ";
-		}
-		case kenbak_state_sk:
-		{
-			return "SK";
-		}
-		case kenbak_state_sl:
-		{
-			return "SL";
-		}
-		case kenbak_state_sm:
-		{
-			return "SM";
-		}
-		case kenbak_state_sn:
-		{
-			return "SN";
-		}
-		case kenbak_state_sp:
-		{
-			return "SP";
-		}
-		case kenbak_state_sq:
-		{
-			return "SQ";
-		}
-		case kenbak_state_sr:
-		{
-			return "SR";
-		}
-		case kenbak_state_ss:
-		{
-			return "SS";
-		}
-		case kenbak_state_st:
-		{
-			return "ST";
-		}
-		case kenbak_state_su:
-		{
-			return "SU";
-		}
-		case kenbak_state_sv:
-		{
-			return "SV";
-		}
-		case kenbak_state_sw:
-		{
-			return "SW";
-		}
-		case kenbak_state_sx:
-		{
-			return "SX";
-		}
-		case kenbak_state_sy:
-		{
-			return "SY";
-		}
-		case kenbak_state_sz:
-		{
-			return "SZ";
-		}
+		case kenbak_state_sa:			{ return "SA"; }
+		case kenbak_state_sb:			{ return "SB"; }
+		case kenbak_state_sc:			{ return "SC"; }
+		case kenbak_state_sd:			{ return "SD"; }
+		case kenbak_state_se:			{ return "SE"; }
+		case kenbak_state_sf:		    { return "SF"; }
+		case kenbak_state_sg:		    { return "SG"; }
+		case kenbak_state_sh:		    { return "SH"; }
+		case kenbak_state_sj:		    { return "SJ"; }
+		case kenbak_state_sk:		    { return "SK"; }
+		case kenbak_state_sl:		    { return "SL"; }
+		case kenbak_state_sm:		    { return "SM"; }
+		case kenbak_state_sn:		    { return "SN"; }
+		case kenbak_state_sp:		    { return "SP"; }
+		case kenbak_state_sq:		    { return "SQ"; }
+		case kenbak_state_sr:		    { return "SR"; }
+		case kenbak_state_ss:		    { return "SS"; }
+		case kenbak_state_st:		    { return "ST"; }
+		case kenbak_state_su:		    { return "SU"; }
+		case kenbak_state_sv:		    { return "SV"; }
+		case kenbak_state_sw:		    { return "SW"; }
+		case kenbak_state_sx:		    { return "SX"; }
+		case kenbak_state_sy:		    { return "SY"; }
+		case kenbak_state_sz:		    { return "SZ"; }
 
 		default:
 		{
