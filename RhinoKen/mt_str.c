462103a940e70dcc06ba537e53fbc47a4a9fdb9d
Marc <RhinoDevel@users.noreply.github.com>
2025-10-16 21:47:34 +0200

    mt_str_fill_with_hex().

--- RhinoKen/mt_str.c
+++ RhinoKen/mt_str.c
@@ -8,6 +8,22 @@
 
 #include "mt_str.h"
 
+void mt_str_fill_with_hex(
+	char * const buf, size_t const buf_len, uint8_t const val)
+{
+	if(buf == NULL)
+	{
+		assert(false);
+		return;
+	}
+	if(buf_len < 2 + 1)
+	{
+		assert(false);
+		return;
+	}
+	snprintf(buf, buf_len, "%02X", (int)val);
+}
+
 void mt_str_fill_with_octal(
 	char * const buf, size_t const buf_len, uint8_t const val)
 {
--- RhinoKen/mt_str.h
+++ RhinoKen/mt_str.h
@@ -9,6 +9,13 @@
 
 /**
  * - Does nothing (debug assertion, only), if NULL given as buffer pointer or
+ *   given buffer length is insufficient (must be at least 2 + 1).
+ */
+void mt_str_fill_with_hex(
+	char * const buf, size_t const buf_len, uint8_t const val);
+
+/**
+ * - Does nothing (debug assertion, only), if NULL given as buffer pointer or
  *   given buffer length is insufficient (must be at least 3 + 1).
  */
 void mt_str_fill_with_octal(
