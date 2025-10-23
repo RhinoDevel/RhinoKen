dfff42023cf5f2108e254e1efb8d970a0f1033fe
Marc <RhinoDevel@users.noreply.github.com>
2025-10-23 17:32:14 +0200

    kenbak_asm_exec(): Modify function head.

--- RhinoKen/kenbak_asm.c
+++ RhinoKen/kenbak_asm.c
@@ -2,10 +2,20 @@
 // Marcel Timm, RhinoDevel, 2025oct23
 
 #include <stdint.h>
+#include <assert.h>
 
 #include "kenbak_asm.h"
 
-uint8_t* kenbak_asm_exec(char const * const txt, size_t const txt_len)
+uint8_t* kenbak_asm_exec(
+	char const * const txt,
+	int const txt_len,
+	char * * const out_msg,
+	int * const out_msg_len)
 {
+	assert(txt != NULL);
+	assert(0 <= txt_len);
+	assert(out_msg != NULL);
+	assert(out_msg_len != NULL);
+
 	return NULL;
 }
--- RhinoKen/kenbak_asm.h
+++ RhinoKen/kenbak_asm.h
@@ -9,7 +9,13 @@
 /**
  * - Returns NULL on error.
  * - Caller takes ownership of returned bytes.
+ * - Assumes that all three input pointers are NOT NULL.
+ * - Caller also takes ownership of returned message, if not NULL.
  */
-uint8_t* kenbak_asm_exec(char const * const txt, size_t const txt_len);
+uint8_t* kenbak_asm_exec(
+	char const * const txt,
+	int const txt_len,
+	char * * const out_msg,
+	int * const out_msg_len);
 
 #endif //KENBAK_ASM
