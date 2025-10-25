e5e3a25efaf11aa92338d22ab022173d9139f854
Marc <RhinoDevel@users.noreply.github.com>
2025-10-25 05:27:20 +0200

    kenbak_asm.c: Add notes ("registers"); Add "memory" and location counter.

--- RhinoKen/kenbak_asm.c
+++ RhinoKen/kenbak_asm.c
@@ -7,8 +7,46 @@
 
 #include "kenbak_asm.h"
 
+// *****************************************************************************
+// *** Notes:                                                                ***
+// *****************************************************************************
+
+// Special memory locations:
+//
+//   0 = A "register".
+//   1 = B "register".
+//   2 = X "register".
+//   3 = P "register".
+// 
+// 128 = Output "register".
+// 129 = "Register" A's overflow (bit 0) and carry (bit 1) flags.
+// 130 = "Register" B's overflow (bit 0) and carry (bit 1) flags.
+// 131 = "Register" X's overflow (bit 0) and carry (bit 1) flags.
+//
+// 255 = Input "register".
+
+// *****************************************************************************
+// *** Error messages and their lengths:                                     ***
+// *****************************************************************************
+
 static char const s_err_not_implemented[] = "Not implemented!";
 static int const s_err_len_not_implemented = sizeof s_err_not_implemented;
+
+// *****************************************************************************
+// *** Functions:                                                            ***
+// *****************************************************************************
+
+static void clear_bytes(uint8_t * const bytes, int const byte_count)
+{
+	// Although would work (see loop, below):
+	assert(bytes != NULL);
+	assert(0 < byte_count);
+
+	for(int i = 0; i < byte_count; ++i)
+	{
+		bytes[i] = 0;
+	}
+}
 
 uint8_t* kenbak_asm_exec(
 	char const * const txt,
@@ -20,6 +58,11 @@ uint8_t* kenbak_asm_exec(
 	assert(0 <= txt_len);
 	assert(out_len != NULL);
 	assert(out_msg != NULL);
+
+	uint8_t mem[256]; // Represents the Kenbak-1's (whole) memory.
+	int pos = 0; // The location counter (initially set to zero automatically).
+
+	clear_bytes(mem, (int)(sizeof mem));
 
 	{
 		assert(sizeof **out_msg == 1);
